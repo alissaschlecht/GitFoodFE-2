58a5bafd49bec843fcdfa0c8360a5c6b63ce963b
Alissa Schlecht <schlecht220@gmail.com>
2019-11-25 11:36:38 -0500

    add react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import ListDishes from './pages/ListDishes';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <p>Git Food</p>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <ListDishes />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/ListDishes/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListDishes = () => {
+  return(
+    <div>
+      <p>List dishes</p>
+    </div>
+  )
+}
+
+export default ListDishes;
