527a2b81e6ba51559e99e6208029bf672eacaa9b
Alissa Schlecht <schlecht220@gmail.com>
2019-11-25 11:42:09 -0500

    add header

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import {
   Link
 } from "react-router-dom";
 import ListDishes from './pages/ListDishes';
+import Header from './components/Header';
 import './App.scss';
 
 function App() {
   return (
+    <>
+    <Header />
     <Router>
       <Switch>
         <Route path="/">
@@ -17,6 +20,7 @@ function App() {
         </Route>
       </Switch>
     </Router>
+    </>
   );
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './header.module.scss';
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <p>Header</p>
+    </div>
+  )
+}
+
+export default Header;
